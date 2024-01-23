d30f8092f31c9d95f9d191d52277f0bc2fd3d2c8
Ian Wong <ianw35091@gmail.com>
2024-01-23 22:49:20 +0800

    Q3 Done

--- PGGE-A2-main/PGGE_A2/Assets/Scripts/Menu.cs
+++ PGGE-A2-main/PGGE_A2/Assets/Scripts/Menu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public AudioSource buttonAudioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +22,20 @@ public class Menu : MonoBehaviour
     {
         //Debug.Log("Loading singleplayer game");
         SceneManager.LoadScene("SinglePlayer");
+        buttonAudioSource.Play();
     }
 
     public void OnClickMultiPlayer()
     {
         //Debug.Log("Loading multiplayer game");
         SceneManager.LoadScene("Multiplayer_Launcher");
+        buttonAudioSource.Play();
+    }
+    public void OnClickBackToMenu()
+    {
+        //Debug.Log("Going back to the menu...");
+        SceneManager.LoadScene("Menu");
+        buttonAudioSource.Play();
     }
 
 }
