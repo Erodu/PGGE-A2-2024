06482930417972e021b76ecc33276fd26d400aaa
Ian Wong <ianw35091@gmail.com>
2024-01-25 13:09:13 +0800

    Commit

--- PGGE-A2-main/PGGE_A2/Assets/Scripts/Menu.cs
+++ PGGE-A2-main/PGGE_A2/Assets/Scripts/Menu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    // Variables for the different button click sound effects.
     public AudioSource buttonMultiPlayerAudioSource;
     public AudioSource buttonSinglePlayerAudioSource;
     public AudioSource buttonBackAudioSource;
@@ -25,6 +26,7 @@ public class Menu : MonoBehaviour
     {
         //Debug.Log("Loading singleplayer game");
         SceneManager.LoadScene("SinglePlayer");
+        // Play Single Player button sound effect.
         buttonSinglePlayerAudioSource.Play();
     }
 
@@ -32,17 +34,20 @@ public class Menu : MonoBehaviour
     {
         //Debug.Log("Loading multiplayer game");
         SceneManager.LoadScene("Multiplayer_Launcher");
+        // Play Multi Player button sound effect.
         buttonMultiPlayerAudioSource.Play();
     }
     public void OnClickBackToMenu()
     {
         //Debug.Log("Going back to the menu...");
         SceneManager.LoadScene("Menu");
+        // Play Back button sound effect.
         buttonBackAudioSource.Play();
     }
 
     public void OnClickJoinServer()
     {
+        // Play Join button sound effect.
         buttonJoinAudioSource.Play();
     }
 
